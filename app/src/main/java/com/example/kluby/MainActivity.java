839448ed8c7698b6a8cecae08b6b8c1b7088284b
Sobol77 <sobotki50@gmail.com>
2022-10-14 11:01:25 +0200

    tworzenie metody findVievById

--- app/src/main/java/com/example/kluby/MainActivity.java
+++ app/src/main/java/com/example/kluby/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.kluby;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.ListView);
+
+
 
     }
 }
