ba213a0c6793a3080bed637ada077d4a34e5ac0a
Sobol77 <sobotki50@gmail.com>
2022-10-17 10:36:54 +0200

    zainplementowanie reszty klubow do arraylisty

--- app/src/main/java/com/example/kluby/MainActivity.java
+++ app/src/main/java/com/example/kluby/MainActivity.java
@@ -18,7 +18,15 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayList<club_object> arrayList = new ArrayList<>();
 
-      arrayList.add(new club_object(R.drawable.psg, "psg", "sub"));
+        arrayList.add(new club_object(R.drawable.psg, "psg", "sub"));
+        arrayList.add(new club_object(R.drawable.arsenal, "arsenal", "sub"));
+        arrayList.add(new club_object(R.drawable.barcelona, "barcelona", "sub"));
+        arrayList.add(new club_object(R.drawable.bvb, "bvb", "sub"));
+        arrayList.add(new club_object(R.drawable.juve, "juventus", "sub"));
+        arrayList.add(new club_object(R.drawable.manchester, "man utd", "sub"));
+        arrayList.add(new club_object(R.drawable.milan, "ac milan", "sub"));
+        arrayList.add(new club_object(R.drawable.real, "real madryt", "sub"));
+
 
     }
 }
