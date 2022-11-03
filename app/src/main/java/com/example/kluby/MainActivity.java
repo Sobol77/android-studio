2d68dca603f87731569772217186fb6e36634e70
Sobol77 <sobotki50@gmail.com>
2022-11-03 10:48:14 +0100

    dodawanie scrollview

--- app/src/main/java/com/example/kluby/MainActivity.java
+++ app/src/main/java/com/example/kluby/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.kluby;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -18,15 +19,18 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayList<club_object> arrayList = new ArrayList<>();
 
-        arrayList.add(new club_object(R.drawable.psg, "psg", "sub"));
-        arrayList.add(new club_object(R.drawable.arsenal, "arsenal", "sub"));
-        arrayList.add(new club_object(R.drawable.barcelona, "barcelona", "sub"));
-        arrayList.add(new club_object(R.drawable.bvb, "bvb", "sub"));
-        arrayList.add(new club_object(R.drawable.juve, "juventus", "sub"));
-        arrayList.add(new club_object(R.drawable.manchester, "man utd", "sub"));
-        arrayList.add(new club_object(R.drawable.milan, "ac milan", "sub"));
-        arrayList.add(new club_object(R.drawable.real, "real madryt", "sub"));
+        arrayList.add(new club_object(R.drawable.psg, "psg", "miasto: paryz"));
+        arrayList.add(new club_object(R.drawable.arsenal, "arsenal", "miasto: Londyn"));
+        arrayList.add(new club_object(R.drawable.barcelona, "barcelona", "miasto: Barcelona"));
+        arrayList.add(new club_object(R.drawable.bvb, "bvb", "miasto: Dortmund"));
+        arrayList.add(new club_object(R.drawable.juve, "juventus", "miasto: Turyn"));
+        arrayList.add(new club_object(R.drawable.manchester, "man utd", "miasto: Manchester"));
+        arrayList.add(new club_object(R.drawable.milan, "ac milan", "miasto: Mediolan"));
+        arrayList.add(new club_object(R.drawable.real, "real madryt", "miasto: Madryt"));
+        arrayList.add(new club_object(R.drawable.manc, "man city", "miasto: Manchester"));
 
+        clubadapter Clubadapter = new clubadapter(this,R.layout.list,arrayList);
 
+        listView.setAdapter(Clubadapter);
     }
 }
